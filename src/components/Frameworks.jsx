cd9a59c4b810076066d892abebfd9374c3b2eefa
AhmadShajahan <ahmadshajahan3000@gmail.com>
2025-07-10 19:31:58 +0530

    tt

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { OrbitingCircles } from "./OrbitingCircles";
 
 export function Frameworks() {
@@ -19,16 +20,18 @@ export function Frameworks() {
     "vitejs",
     "wordpress",
   ];
+
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={`outer-${index}`} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
+
       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
-        {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        {[...skills].reverse().map((skill, index) => (
+          <Icon key={`inner-${index}`} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
     </div>
@@ -36,5 +39,10 @@ export function Frameworks() {
 }
 
 const Icon = ({ src }) => (
-  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+  <img src={src} alt="" className="duration-200 rounded-sm hover:scale-110" />
 );
+
+// ✅ Add prop validation to fix ESLint warning
+Icon.propTypes = {
+  src: PropTypes.string.isRequired,
+};
