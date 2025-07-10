fb146b607c70cb79ca37a14293438ce65c41cfa6
AhmadShajahan <ahmadshajahan3000@gmail.com>
2025-07-10 20:34:07 +0530

    tt

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from "@tailwindcss/vite";
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          lottie: ['lottie-react'], // add more if needed
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           react: ['react', 'react-dom'],
+//           lottie: ['lottie-react'], // add more if needed
+//         },
+//       },
+//     },
+//   },
+// });
